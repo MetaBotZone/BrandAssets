b55b05db96cc91d95086a091a22ed7428c294039
Ruben <just14zy@gmail.com>
2024-08-15 08:23:55 +0700

    nft

---
+++ metabot-collection/data/script.sh
@@ -0,0 +1,84 @@
+ for i in {1..9}
+do
+    echo '{
+    "name": "{ MetaSonic #000'$i'}",
+    "description": "Meet your MetaSonic, a unique digital collectible from the 1ˢᵗ Edition MetaBot collection.
+    Each MetaSonic is designed for use in MetaBot Metaverse games and comes with special abilities that enhance your gameplay.
+As you progress, your MetaSonic can evolve, unlocking new skills and forms.
+This character is part of a limited series of 1000, with potential for future editions.
+Own, evolve, and enjoy adventures with your one-of-a-kind MetaSonic.",
+    "image": "https://rubeton.app/img/MetaSonicFile.png",
+    "buttons": [
+        {
+            "label": "Website",
+            "uri": "https://metabot.zone/"
+        },
+        {
+            "label": "Chat",
+            "uri": "https://t.me/metabotzone"
+        },
+        {
+            "label": "Play",
+            "uri": "https://metabot.zone/Projects/MetabotLobby"
+        }
+    ],
+    "attributes": []
+}' > $((i-1))'.json'
+done
+
+
+ for i in {10..99}
+do
+    echo '{
+    "name": "{ MetaSonic #00'$i'}",
+    "description": "Meet your MetaSonic, a unique digital collectible from the 1ˢᵗ Edition MetaBot collection.
+    Each MetaSonic is designed for use in MetaBot Metaverse games and comes with special abilities that enhance your gameplay.
+As you progress, your MetaSonic can evolve, unlocking new skills and forms.
+This character is part of a limited series of 1000, with potential for future editions.
+Own, evolve, and enjoy adventures with your one-of-a-kind MetaSonic.",
+    "image": "https://rubeton.app/img/MetaSonicFile.png",
+    "buttons": [
+        {
+            "label": "Website",
+            "uri": "https://metabot.zone/"
+        },
+        {
+            "label": "Chat",
+            "uri": "https://t.me/metabotzone"
+        },
+        {
+            "label": "Play",
+            "uri": "https://metabot.zone/Projects/MetabotLobby"
+        }
+    ],
+    "attributes": []
+}' > $((i-1))'.json'
+done
+
+for i in {100..250}
+do
+    echo '{
+    "name": "{ MetaSonic #0'$i'}",
+    "description": "Meet your MetaSonic, a unique digital collectible from the 1ˢᵗ Edition MetaBot collection.
+    Each MetaSonic is designed for use in MetaBot Metaverse games and comes with special abilities that enhance your gameplay.
+As you progress, your MetaSonic can evolve, unlocking new skills and forms.
+This character is part of a limited series of 1000, with potential for future editions.
+Own, evolve, and enjoy adventures with your one-of-a-kind MetaSonic.",
+    "image": "https://rubeton.app/img/MetaSonicFile.png",
+    "buttons": [
+        {
+            "label": "Website",
+            "uri": "https://metabot.zone/"
+        },
+        {
+            "label": "Chat",
+            "uri": "https://t.me/metabotzone"
+        },
+        {
+            "label": "Play",
+            "uri": "https://metabot.zone/Projects/MetabotLobby"
+        }
+    ],
+    "attributes": []
+}' > $((i-1))'.json'
+done
