fd7069fd496a8e123840bcf79a18e7ae3d74e48d
Ruben <just14zy@gmail.com>
2024-08-15 10:24:56 +0700

    new urls

--- metabot-collection/data/script.sh
+++ metabot-collection/data/script.sh
@@ -1,13 +1,13 @@
  for i in {1..9}
 do
     echo '{
-    "name": "{ MetaSonic #000'$i'}",
+    "name": "{ MetaSonic #000'$i' }",
     "description": "Meet your MetaSonic, a unique digital collectible from the 1ˢᵗ Edition MetaBot collection.
     Each MetaSonic is designed for use in MetaBot Metaverse games and comes with special abilities that enhance your gameplay.
 As you progress, your MetaSonic can evolve, unlocking new skills and forms.
 This character is part of a limited series of 1000, with potential for future editions.
 Own, evolve, and enjoy adventures with your one-of-a-kind MetaSonic.",
-    "image": "https://rubeton.app/img/MetaSonicFile.png",
+    "image": "https://raw.githubusercontent.com/MetaBotZone/BrandAssets/main/metabot-collection/card.png",
     "buttons": [
         {
             "label": "Website",
@@ -30,13 +30,13 @@ done
  for i in {10..99}
 do
     echo '{
-    "name": "{ MetaSonic #00'$i'}",
+    "name": "{ MetaSonic #00'$i' }",
     "description": "Meet your MetaSonic, a unique digital collectible from the 1ˢᵗ Edition MetaBot collection.
     Each MetaSonic is designed for use in MetaBot Metaverse games and comes with special abilities that enhance your gameplay.
 As you progress, your MetaSonic can evolve, unlocking new skills and forms.
 This character is part of a limited series of 1000, with potential for future editions.
 Own, evolve, and enjoy adventures with your one-of-a-kind MetaSonic.",
-    "image": "https://rubeton.app/img/MetaSonicFile.png",
+    "image": "https://raw.githubusercontent.com/MetaBotZone/BrandAssets/main/metabot-collection/card.png",
     "buttons": [
         {
             "label": "Website",
@@ -58,13 +58,13 @@ done
 for i in {100..250}
 do
     echo '{
-    "name": "{ MetaSonic #0'$i'}",
+    "name": "{ MetaSonic #0'$i' }",
     "description": "Meet your MetaSonic, a unique digital collectible from the 1ˢᵗ Edition MetaBot collection.
     Each MetaSonic is designed for use in MetaBot Metaverse games and comes with special abilities that enhance your gameplay.
 As you progress, your MetaSonic can evolve, unlocking new skills and forms.
 This character is part of a limited series of 1000, with potential for future editions.
 Own, evolve, and enjoy adventures with your one-of-a-kind MetaSonic.",
-    "image": "https://rubeton.app/img/MetaSonicFile.png",
+    "image": "https://raw.githubusercontent.com/MetaBotZone/BrandAssets/main/metabot-collection/card.png",
     "buttons": [
         {
             "label": "Website",
