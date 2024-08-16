630b0249d08729da0a1839571671118d824edf2e
Ruben <just14zy@gmail.com>
2024-08-16 15:05:05 +0700

    1 000 units

--- metabot-collection/data/script.sh
+++ metabot-collection/data/script.sh
@@ -70,3 +70,48 @@ do
     "attributes": []
 }' > $((i-1))'.json'
 done
+
+for i in {251..999}
+do
+    echo '{
+    "name": "{ MetaSonic #0'$i' }",
+    "description": "Meet your MetaSonic, a unique digital collectible from the 1ˢᵗ Edition MetaBot collection.\\nEach MetaSonic is designed for use in MetaBot Metaverse games and comes with special abilities that enhance your gameplay.\\nAs you progress, your MetaSonic can evolve, unlocking new skills and forms.\\nThis character is part of a limited series of 1000, with potential for future editions.\\nOwn, evolve, and enjoy adventures with your one-of-a-kind MetaSonic.",
+    "image": "https://raw.githubusercontent.com/MetaBotZone/BrandAssets/main/metabot-collection/card.png",
+    "buttons": [
+        {
+            "label": "Website",
+            "uri": "https://metabot.zone/"
+        },
+        {
+            "label": "Chat",
+            "uri": "https://t.me/metabotzone"
+        },
+        {
+            "label": "Play",
+            "uri": "https://metabot.zone/Projects/MetabotLobby"
+        }
+    ],
+    "attributes": []
+}' > $((i-1))'.json'
+done
+
+    echo '{
+    "name": "{ MetaSonic #1000 }",
+    "description": "Meet your MetaSonic, a unique digital collectible from the 1ˢᵗ Edition MetaBot collection.\\nEach MetaSonic is designed for use in MetaBot Metaverse games and comes with special abilities that enhance your gameplay.\\nAs you progress, your MetaSonic can evolve, unlocking new skills and forms.\\nThis character is part of a limited series of 1000, with potential for future editions.\\nOwn, evolve, and enjoy adventures with your one-of-a-kind MetaSonic.",
+    "image": "https://raw.githubusercontent.com/MetaBotZone/BrandAssets/main/metabot-collection/card.png",
+    "buttons": [
+        {
+            "label": "Website",
+            "uri": "https://metabot.zone/"
+        },
+        {
+            "label": "Chat",
+            "uri": "https://t.me/metabotzone"
+        },
+        {
+            "label": "Play",
+            "uri": "https://metabot.zone/Projects/MetabotLobby"
+        }
+    ],
+    "attributes": []
+}' > '999.json'
